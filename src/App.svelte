8de13ad4780672a9d8f33eea1c546410bc9fcb2d
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2023-01-17 11:34:24 +0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,112 @@
+<script lang="ts">
+  import svelteLogo from "./assets/svelte.svg";
+  import Nested from "./components/Nested.svelte";
+
+  const name = "Svelte";
+  const stringHTML = "this string contains some <strong>HTML!!!</strong>";
+
+  let count = 0;
+  function incrementCount() {
+    count += 1;
+  }
+  function decrementCount() {
+    count -= 1;
+  }
+  $: doubled = count * 2;
+  $: {
+    console.log("the count is " + count);
+    if (count > 0) alert("I SAID THE COUNT IS " + count);
+  }
+  $: if (count >= 10) {
+    alert("count is dangerously high!");
+    count = 9;
+  }
+
+  let numbers = [1, 2, 3, 4];
+  function addNumber() {
+    numbers.push(numbers.length + 1);
+    numbers = numbers;
+  }
+  $: sum = numbers.reduce((t, n) => t + n, 0);
+</script>
+
+<main>
+  <h1>Learn Svelte</h1>
+  <ol>
+    <li>
+      <h2>Introduction</h2>
+      <ol>
+        <li>
+          <h3>Basics</h3>
+          <p>Hello world!</p>
+        </li>
+        <li>
+          <h3>Adding data</h3>
+          <p>Hello {name}!</p>
+          <p>Hello {name.toUpperCase()}!</p>
+        </li>
+        <li>
+          <h3>Dynamic attributes</h3>
+          <img src="./src/assets/svelte.svg" alt="Svelte logo" />
+          <img src={svelteLogo} alt="Svelte logo" />
+        </li>
+        <li>
+          <h3>Styling</h3>
+          <p class="first-paragraph">This is styled paragraph.</p>
+        </li>
+        <li>
+          <h3>Nested components</h3>
+          <Nested />
+        </li>
+        <li>
+          <h3>HTML tags</h3>
+          <p>{stringHTML}</p>
+          <p>{@html stringHTML}</p>
+        </li>
+      </ol>
+    </li>
+    <li>
+      <h2>Reactivity</h2>
+      <ol>
+        <li>
+          <h3>Assignments</h3>
+          <p>Count is {count}</p>
+          <button on:click={incrementCount}> Increment count </button>
+          <button on:click={decrementCount}> Decrement count </button>
+        </li>
+        <li>
+          <h3>Declarations</h3>
+          <p>{count} doubled is {count * 2}</p>
+          <p>{count} doubled is {doubled}</p>
+        </li>
+        <li>
+          <h3>Statements</h3>
+          <pre>
+            {`
+            $: {
+              console.log("the count is " + count);
+              if (count > 0) alert("I SAID THE COUNT IS " + count);
+            }
+            $: if (count >= 10) {
+              alert("count is dangerously high!");
+              count = 9;
+            }
+            `}
+          </pre>
+        </li>
+        <li>
+          <h3>Updating arrays and objects</h3>
+          <p>{numbers.join(" + ")} = {sum}</p>
+          <button on:click={addNumber}> Add a number </button>
+        </li>
+      </ol>
+    </li>
+  </ol>
+</main>
+
+<style>
+  p.first-paragraph {
+    color: orangered;
+    font-size: 2em;
+  }
+</style>
